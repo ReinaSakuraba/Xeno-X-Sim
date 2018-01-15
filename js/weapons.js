ca273a818b0f5a3a6f59392a74adb9bf9b29290b
ReinaSakuraba <juniorterra7@gmail.com>
2018-01-14 22:36:44 -0700

    Add default weapons

--- js/weapons.js
+++ js/weapons.js
@@ -2,66 +2,291 @@ var weapons = {
     longsword: {
         name: "Longsword",
         type: "Melee",
-        weapons: {}
+        weapons: {
+            combatLongsword: {
+                name: "Combat Longsword",
+                type: "Longsword",
+                rarity: "Common",
+                attack: 48,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 2.0,
+                ammo: 0,
+                tpGain: 230,
+                stability: 8,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     shield: {
         name: "Shield",
         type: "Melee",
-        weapons: {}
+        weapons: {
+            combatShield: {
+                name: "Combat Shield",
+                type: "Shield",
+                rarity: "Common",
+                attack: 80,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 3.0,
+                ammo: 0,
+                tpGain: 165,
+                stability: 12,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     dualSwords: {
         name: "Dual Swords",
         type: "Melee",
-        weapons: {}
+        weapons: {
+            combatDualSwords: {
+                name: "Combat Dual Swords",
+                type: "Dual Swords",
+                rarity: "Common",
+                attack: 16,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 1.5,
+                ammo: 0,
+                tpGain: 72,
+                stability: 5,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     javelin: {
         name: "Javelin",
         type: "Melee",
-        weapons: {}
+        weapons: {
+            combatJavelin: {
+                name: "Combat Javelin",
+                type: "Javelin",
+                rarity: "Common",
+                attack: 78,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 2.5,
+                ammo: 0,
+                tpGain: 140,
+                stability: 10,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     knife: {
         name: "Knife",
         type: "Melee",
-        weapons: {}
+        weapons: {
+            combatKnife: {
+                name: "Combat Knife",
+                type: "Knife",
+                rarity: "Common",
+                attack: 10,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 1.0,
+                ammo: 0,
+                tpGain: 88,
+                stability: 10,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     photonSaber: {
         name: "Photon Saber",
         type: "Melee",
-        weapons: {}
+        weapons: {
+            combatPhotonSaber: {
+                name: "Combat Photon Saber",
+                type: "Photon Saber",
+                rarity: "Common",
+                attack: 40,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Beam",
+                cooldown: 1.5,
+                ammo: 0,
+                tpGain: 50,
+                stability: 15,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     assaultRifle: {
         name: "Assault Rifle",
         type: "Ranged",
-        weapons: {}
+        weapons: {
+            combatAssaultRifle: {
+                name: "Combat Assault Rifle",
+                type: "Assault Rifle",
+                rarity: "Common",
+                attack: 3,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 2.7,
+                ammo: 20,
+                tpGain: 1,
+                stability: 12,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     gatlingGun: {
         name: "Gatling Gun",
         type: "Ranged",
-        weapons: {}
+        weapons: {
+            combatGatlingGun: {
+                name: "Combat Gatling Gun",
+                type: "Gatling Gun",
+                rarity: "Common",
+                attack: 5,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 6.0,
+                ammo: 36,
+                tpGain: 1,
+                stability: 12,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     dualGuns: {
         name: "Dual Guns",
         type: "Ranged",
-        weapons: {}
+        weapons: {
+            combatDualGuns: {
+                name: "Combat Dual Guns",
+                type: "Dual Guns",
+                rarity: "Common",
+                attack: 6,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 2.0,
+                ammo: 8,
+                tpGain: 2,
+                stability: 8,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     sniperRifle: {
         name: "Sniper Rifle",
         type: "Ranged",
-        weapons: {}
+        weapons: {
+            combatSniperRifle: {
+                name: "Combat Sniper Rifle",
+                type: "Sniper Rifle",
+                rarity: "Common",
+                attack: 28,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 4.0,
+                ammo: 5,
+                tpGain: 7,
+                stability: 3,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     raygun: {
         name: "Raygun",
         type: "Ranged",
-        weapons: {}
+        weapons: {
+            combatRaygun: {
+                name: "Combat Raygun",
+                type: "Raygun",
+                rarity: "Common",
+                attack: 32,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Beam",
+                cooldown: 3.7,
+                ammo: 4,
+                tpGain: 9,
+                stability: 20,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     psychoLaunchers: {
         name: "Psycho Launchers",
         type: "Ranged",
-        weapons: {}
+        weapons: {
+            combatPsychoLaunchers: {
+                name: "Combat Psycho Launchers",
+                type: "Psycho Launchers",
+                rarity: "Common",
+                attack: 8,
+                level: 1,
+                maker: "Sakuraba Industries",
+                attribute: "Physical",
+                cooldown: 2.0,
+                ammo: 6,
+                tpGain: 3,
+                stability: 8,
+                upgrades: 0,
+                innateTraits: {},
+                sellPrice: 0
+            }
+        }
     },
     multigun: {
         name: "Multigun",
         type: "Ranged",
-        weapons: {}
+        weapons: {
+            deltaBliss: {
+                name: "Delta Bliss",
+                type: "Multigun",
+                rarity: "Prime",
+                attack: 56,
+                level: 60,
+                maker: "Factory 1.21",
+                attribute: "Ether",
+                cooldown: 2.0,
+                ammo: 12,
+                tpGain: 30,
+                stability: 30,
+                upgrades: 3,
+                innateTraits: {
+                    destabilizer: 7,
+                    etherAttackPlus: 7,
+                    openingArtDamageUp: 7
+                },
+                sellPrice: 4125
+            }
+        }
     }
 };
